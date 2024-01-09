a29e2a2b92e06e3a3fc445afdeb23849198f5092
Jonas Lang <jonaslang@jonaslang.de>
2024-01-10 00:06:25 +0100

    fixed lints

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import os
 import shutil
 import py2exe
 
-folder = './dist'
-for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
+FOLDER = './dist'
+for filename in os.listdir(FOLDER):
+    file_path = os.path.join(FOLDER, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
         elif os.path.isdir(file_path):
             shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    except (FileExistsError,  FileNotFoundError) as e:
+        print(f'Failed to delete {file_path}. Reason: {e}')
 
 py2exe.freeze(console=[{'script': 'main.py'}],
               options={'py2exe': {'bundle_files': 1, 'compressed': True}})
